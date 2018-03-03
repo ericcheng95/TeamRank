8f853a1ced1505ce78b554003eda6df3472426ce
Eric Cheng <ericcheng95@gmail.com>
2018-03-03 18:33:14 -0500

    maybe working, kinda weird

--- teamrank.py
+++ teamrank.py
@@ -4,8 +4,12 @@ import sys
 
 teamsdict = {}
 teamsdictreverse = {}
-teamsmatrix = None
 resultsdict = {}
+
+teamssetwins = {}
+teamssetlosses = {}
+teamsmatchwins = {}
+teamsmatchlosses = {}
 
 def print_results(b):
   teamnumber = 0
@@ -15,11 +19,30 @@ def print_results(b):
   sortedresultsdict = sorted(resultsdict.items(), key=operator.itemgetter(1), reverse = True)
   place = 1
   for sortedresult in sortedresultsdict:
-    print(place, sortedresult)
+    teamname = sortedresult[0]
+    value = sortedresult[1]
+    setwins = str(teamssetwins[teamname])
+    setlosses = str(teamssetlosses[teamname])
+    matchwins = str(teamsmatchwins[teamname])
+    matchlosses = str(teamsmatchlosses[teamname])
+
+    placestring = str(place)
+    placestring = "{:3}".format(placestring)
+    teamname = "{:30}".format(teamname)
+    valuestring = str(value)
+    valuestring = "{:30}".format(valuestring)
+    setwins = "{:3}".format(setwins)
+    setlosses = "{:3}".format(setlosses)
+    matchwins = "{:3}".format(matchwins)
+    matchlosses = "{:3}".format(matchlosses)
+
+    print(placestring, teamname, valuestring, "Set", setwins, ":", setlosses, "Match", matchwins, ":", matchlosses)
     place += 1
 
 def power_iteration(A, numsimulations):
-  b_k = numpy.random.rand(A.shape[0])
+  #b_k = numpy.random.rand(A.shape[0])
+  b_k = numpy.ones(A.shape[0])
+  #b_k = numpy.linalg.norm(b_k)
 
   for _ in range(numsimulations):
     b_k1 = numpy.dot(A, b_k)
@@ -30,7 +53,32 @@ def power_iteration(A, numsimulations):
 
   return b_k
 
-def add_match_data(data):
+def power_iteration2(A):
+  eig = numpy.linalg.eig(A)
+  eigenvalues = eig[0]
+  eigenvectors = eig[1]
+  largesteigenvalue = 0
+  largesteigenvalueindex = 0
+  index = 0
+  for eigenvalue in eigenvalues:
+    if (abs(eigenvalue) > largesteigenvalue):
+      largesteigenvalue = eigenvalue
+      largesteigenvalueindex = index
+    index += 1
+  print(largesteigenvalue)
+  print(eigenvectors[largesteigenvalueindex])
+
+def normalize_matrix(teamsmatrix):
+  teamsmatrix /= numpy.sum(teamsmatrix, axis=0)
+  return teamsmatrix
+
+def add_match_score(dictionary, teamname, value):
+  if (teamname in dictionary):
+    dictionary[teamname] += value
+  else:
+    dictionary[teamname] = value
+
+def add_match_data(teamsmatrix, data):
   team1 = data[0]
   team2 = data[1]
 
@@ -48,13 +96,24 @@ def add_match_data(data):
   team2number = teamsdict[team2]
   team1score = int(data[2])
   team2score = int(data[3])
+  numgames = team1score + team2score
 
-  global teamsmatrix
-  teamsmatrix[team1number, team2number] += team1score
-  teamsmatrix[team2number, team2number] += team2score
+  teamsmatrix[team1number, team2number] = team1score  * 1.0/ numgames
+  teamsmatrix[team2number, team1number] = team2score * 1.0 / numgames
+  
+  add_match_score(teamsmatchwins, team1, team1score)
+  add_match_score(teamsmatchwins, team2, team2score)
+  add_match_score(teamsmatchlosses, team1, team2score)
+  add_match_score(teamsmatchlosses, team2, team1score)
+
+  add_match_score(teamssetwins, team1, int(team1score > team2score))
+  add_match_score(teamssetlosses, team1, int(team2score > team1score))
+  add_match_score(teamssetwins, team2, int(team2score > team1score))
+  add_match_score(teamssetlosses, team2, int(team1score > team2score))
+  
 
 # Opens the match file that is specified, and adds the data to the teamsmatrix
-def initialize_match(matchnumber):
+def initialize_match(teamsmatrix, matchnumber):
   matchstring = str(matchnumber).zfill(2)
   matchfilename = "Rounds/Round" + matchstring
   with open (matchfilename, "r") as matchesfile:
@@ -63,12 +122,12 @@ def initialize_match(matchnumber):
     for match in matches:
       match = match.rstrip()
       data = match.split(":")
-      add_match_data(data)
+      add_match_data(teamsmatrix, data)
 
 # Initializes each match that is specified
-def initialize_matches(startindex, endindex):
+def initialize_matches(teamsmatrix, startindex, endindex):
   for i in range(startindex, endindex + 1):
-    initialize_match(i)
+    initialize_match(teamsmatrix, i)
 
 # Opens Teams file and initiates the dict and matrix
 def initialize_teams_dict():
@@ -80,10 +139,9 @@ def initialize_teams_dict():
       teamsdict[team] = teamnumber
       teamsdictreverse[teamnumber] = team
       teamnumber = teamnumber + 1
-    global teamsmatrix
-    teamsmatrix = numpy.zeros((teamnumber, teamnumber))
-
-# Calls the Functions
+    return teamnumber
+ 
+ # Calls the Functions
 def main():
   # Converts arguments to ints (breaks if invalid)
   startindex = int(sys.argv[1])
@@ -94,13 +152,16 @@ def main():
     return
 
   # Creates the Teams Dict from the Teams file
-  initialize_teams_dict()
+  teamnumber = initialize_teams_dict()
+  teamsmatrix = numpy.eye(teamnumber)
+
   # Creates the Teams and Matches Matrix
-  initialize_matches(startindex, endindex)
+  initialize_matches(teamsmatrix, startindex, endindex)
 
   # Calculates Eigenvectors
-  global teamsmatrix
-  b = power_iteration(teamsmatrix, 1)
+  teamsmatrix = normalize_matrix(teamsmatrix)
+  b = power_iteration(teamsmatrix, 10000)
   print_results(b)
+  #power_iteration2(teamsmatrix)
 
 main()
